e4dcf49eb413fa326574ceb85076faf21d42cbc7
Muhammad Rizqi <mrizqi070502@gmail.com>
2023-09-12 16:49:12 +0700

    create project structure and model

---
+++ Momo/data/model/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Momo.data.model
+{
+    class Task
+    {
+        String id;
+        String name;
+        String description;
+        bool isCompleted;
+
+        Task(String id, String name, String description)
+        {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.isCompleted = false;
+        }
+    }
+}
---
+++ Momo/data/model/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Momo.data.model
+{
+    class User
+    {
+        String id;
+        String name;
+        String email;
+        String password;
+
+        User(string id, string name, string email, string password)
+        {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+            this.password = password;
+        }
+    }
+}
