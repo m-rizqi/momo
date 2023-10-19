391a2ce49d8aa59a35132fd8bcaa8c41e409b842
Muhammad Rizqi <mrizqi070502@gmail.com>
2023-10-19 14:28:06 +0700

    feat: login screen

--- Momo/presentation/login/LoginPage.xaml.cs
+++ Momo/presentation/login/LoginPage.xaml.cs
@@ -23,5 +23,10 @@ namespace Momo.presentation.login
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
