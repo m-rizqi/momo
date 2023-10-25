1587116ce32711a1623dfa97465797d9c6169159
xjagaimo <nathanpk1902@gmail.com>
2023-10-25 15:39:17 +0700

    add homepage

--- Momo/presentation/home/HomePage.xaml.cs
+++ Momo/presentation/home/HomePage.xaml.cs
@@ -23,5 +23,10 @@ namespace Momo.presentation.home
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
